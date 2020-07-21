226c40d97151e879bbcdb8627bdbb0c983d00896
Toko_dev <layesall05@gmail.com>
2020-07-21 19:48:43 +0000

    Authentification jwt qui marche

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,9 +7,16 @@ use App\Entity\User;
 use App\Entity\Profil;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
     public function load(ObjectManager $manager)
     {
         $faker = Faker\Factory::create('fr_FR');
@@ -18,9 +25,10 @@ class AppFixtures extends Fixture
             $profil->setLibelle($faker->unique()->randomElement(['Admin','Formateur','CM']));
             $manager->persist($profil);
             $user = new User();
+            $harsh = $this->encoder->encodePassword($user, 'sonatel');
             $user->setProfil($profil);
             $user->setUsername($faker->unique()->randomElement(['babacar','aminata','Oumar']));
-            $user->setPassword($faker->randomElement(['sonatel','sonatel','sonatel']));
+            $user->setPassword($faker->randomElement([$harsh,$harsh,$harsh]));
             $user->setPrenom($faker->randomElement(['babacar','aminata','Oumar']));
             $user->setNom($faker->randomElement(['Diouf','Lo','Enne']));
             $user->setEmail($faker->randomElement(['babacar@sa.sn','aminata@sa.sn','Oumar@sa.sn']));
