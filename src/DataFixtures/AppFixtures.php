b7589d49692325c14d52034df67354b6e24ae5df
unknown <laminata463@gmail.com>
2020-07-22 02:13:40 +0000

    amina

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\DataFixtures;
+use App\Entity\User;
+use App\Entity\Profil;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+class AppFixtures extends Fixture
+{
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $faker = Faker\Factory::create('fr_FR');
+        for($i=0; $i<3; $i++){
+            $profil = new Profil();
+            $profil->setLibelle($faker->unique()->randomElement(['Admin','Formateur','CM']));
+            $manager->persist($profil);
+            $user = new User();
+            $harsh = $this->encoder->encodePassword($user, 'sonatel');
+            $user->setProfil($profil);
+            $user->setUsername($faker->unique()->randomElement(['babacar','aminata','Oumar']));
+            $user->setPassword($faker->randomElement([ $harsh, $harsh, $harsh]));
+            $user->setPrenom($faker->randomElement(['babacar','aminata','Oumar']));
+            $user->setNom($faker->randomElement(['Diouf','Lo','Enne']));
+            $user->setEmail($faker->randomElement(['babacar@sa.sn','aminata@sa.sn','Oumar@sa.sn']));
+            $user->setTel($faker->randomElement(['778458574','778548596','774859652']));
+            $user->setAvatar('avatar');
+            $user->setRoles(['ROLE_USER']);
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Competence.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\CompetenceRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=CompetenceRepository::class)
+ */
+class Competence
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $libelle;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Apprenant::class, mappedBy="competence")
+     */
+    private $apprenants;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Tag::class, inversedBy="competences")
+     */
+    private $tag;
+
+    public function __construct()
+    {
+        $this->apprenants = new ArrayCollection();
+        $this->tag = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Apprenant[]
+     */
+    public function getApprenants(): Collection
+    {
+        return $this->apprenants;
+    }
+
+    public function addApprenant(Apprenant $apprenant): self
+    {
+        if (!$this->apprenants->contains($apprenant)) {
+            $this->apprenants[] = $apprenant;
+            $apprenant->addCompetence($this);
+        }
+
+        return $this;
+    }
+
+    public function removeApprenant(Apprenant $apprenant): self
+    {
+        if ($this->apprenants->contains($apprenant)) {
+            $this->apprenants->removeElement($apprenant);
+            $apprenant->removeCompetence($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Tag[]
+     */
+    public function getTag(): Collection
+    {
+        return $this->tag;
+    }
+
+    public function addTag(Tag $tag): self
+    {
+        if (!$this->tag->contains($tag)) {
+            $this->tag[] = $tag;
+        }
+
+        return $this;
+    }
+
+    public function removeTag(Tag $tag): self
+    {
+        if ($this->tag->contains($tag)) {
+            $this->tag->removeElement($tag);
+        }
+
+        return $this;
+    }
+}
